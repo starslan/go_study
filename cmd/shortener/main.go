001d83593621acf04a7fd6a0ca957a521aabbebc
rstarostenko <starslan-1@yandex.ru>
2023-12-12 13:17:10 +0300

    check test

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -1,5 +1,15 @@
 package main
 
+import (
+	"fmt"
+	"net/http"
+)
+
 func main() {
-	//http.ListenAndServe(":8080", nil)
+	resp, err := http.Get("http://example.com/")
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	fmt.Println(resp)
 }
