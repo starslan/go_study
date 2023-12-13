ec678e441198a364d682d14c93300120225c399d
rstarostenko <starslan-1@yandex.ru>
2023-12-13 10:35:53 +0300

    changed project struct

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -1,14 +1,14 @@
 package main
 
 import (
-	"go_study/internal/app/httpServer"
+	"go_study/internal/app/httpserver"
 	"net/http"
 )
 
 func main() {
 
 	shortURLList := make(map[string]string)
-	http.HandleFunc("/", httpServer.ShortURLHandler(shortURLList))
+	http.HandleFunc("/", httpserver.ShortURLHandler(shortURLList))
 	err := http.ListenAndServe(":8080", nil)
 	if err != nil {
 		return
