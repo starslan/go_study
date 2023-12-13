43df9268b32995c72bb2f38ae5c75dcc886e53ac
rstarostenko <starslan-1@yandex.ru>
2023-12-13 10:24:54 +0300

    changed project struct

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -1,55 +1,14 @@
 package main
 
 import (
-	"fmt"
-	"io"
+	"go_study/internal/app/http-server"
 	"net/http"
-	"strconv"
-	"strings"
 )
-
-//var shortURLList map[string]string
-
-func addShortURL(url []byte, shortURLList map[string]string) string {
-
-	var key = strconv.Itoa(len(shortURLList) + 1)
-	shortURLList[key] = string(url)
-	return key
-}
-
-func shortURLHandler(shortURLList map[string]string) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
-
-		switch r.Method {
-		case http.MethodGet:
-			if val, ok := shortURLList[strings.Trim(r.RequestURI, "/")]; ok {
-				w.Header().Set("Location", val)
-				w.WriteHeader(http.StatusTemporaryRedirect)
-			} else {
-				w.WriteHeader(http.StatusBadRequest)
-			}
-
-		case http.MethodPost:
-			defer r.Body.Close()
-			payload, err := io.ReadAll(r.Body)
-			if err != nil {
-				fmt.Println(err)
-			}
-
-			w.WriteHeader(http.StatusCreated)
-			var link = "http://" + r.Host + "/" + addShortURL(payload, shortURLList)
-			w.Write([]byte(link))
-
-		}
-	}
-
-}
 
 func main() {
 
 	shortURLList := make(map[string]string)
-	shortURLHandler(shortURLList)
-	http.HandleFunc("/", shortURLHandler(shortURLList))
+	http.HandleFunc("/", http_server.ShortURLHandler(shortURLList))
 	err := http.ListenAndServe(":8080", nil)
 	if err != nil {
 		return
---
+++ internal/app/http-server/HandlerHTTP.go
@@ -0,0 +1,45 @@
+package http_server
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"strconv"
+	"strings"
+)
+
+var shortURLList map[string]string
+
+func addShortURL(url []byte, shortURLList map[string]string) string {
+
+	var key = strconv.Itoa(len(shortURLList) + 1)
+	shortURLList[key] = string(url)
+	return key
+}
+
+func ShortURLHandler(shortURLList map[string]string) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		switch r.Method {
+		case http.MethodGet:
+			if val, ok := shortURLList[strings.Trim(r.RequestURI, "/")]; ok {
+				w.Header().Set("Location", val)
+				w.WriteHeader(http.StatusTemporaryRedirect)
+			} else {
+				w.WriteHeader(http.StatusBadRequest)
+			}
+
+		case http.MethodPost:
+			defer r.Body.Close()
+			payload, err := io.ReadAll(r.Body)
+			if err != nil {
+				fmt.Println(err)
+			}
+
+			w.WriteHeader(http.StatusCreated)
+			var link = "http://" + r.Host + "/" + addShortURL(payload, shortURLList)
+			w.Write([]byte(link))
+
+		}
+	}
+
+}
