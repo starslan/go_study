20ef908b352bfe1d9a00727f5128a3a1d55affe2
Ruslan <starslan-1@yandex.ru>
2023-12-13 10:38:08 +0300

    check test (#1)
    * check test
    
    * check test
    
    * check test
    
    * check test
    
    * fix
    
    * fix
    
    * changed project struct
    
    * changed project struct
    
    * changed project struct

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -1,3 +1,17 @@
 package main
 
-func main() {}
+import (
+	"go_study/internal/app/httpserver"
+	"net/http"
+)
+
+func main() {
+
+	shortURLList := make(map[string]string)
+	http.HandleFunc("/", httpserver.ShortURLHandler(shortURLList))
+	err := http.ListenAndServe(":8080", nil)
+	if err != nil {
+		return
+	}
+
+}
