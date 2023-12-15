dfcb59f2811f24ee09230700429241ad9c3162f8
rstarostenko <starslan-1@yandex.ru>
2023-12-15 13:00:11 +0300

    iter2

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -1,14 +1,14 @@
 package main
 
 import (
-	"go_study/internal/app/httpserver"
+	"go_study/internal/app/httpserver/handlers"
 	"net/http"
 )
 
 func main() {
 
 	shortURLList := make(map[string]string)
-	http.HandleFunc("/", httpserver.ShortURLHandler(shortURLList))
+	http.HandleFunc("/", handlers.ShortURLHandler(shortURLList))
 	err := http.ListenAndServe(":8080", nil)
 	if err != nil {
 		return
