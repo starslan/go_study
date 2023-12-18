4a77a4227ab0f2fa65482e929cf79d2775add422
rstarostenko <starslan-1@yandex.ru>
2023-12-18 10:16:03 +0300

    iter3

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"github.com/go-chi/chi/v5"
 	"go_study/internal/app/httpserver/handlers"
 	"net/http"
 )
@@ -8,8 +9,11 @@ import (
 func main() {
 
 	shortURLList := make(map[string]string)
-	http.HandleFunc("/", handlers.ShortURLHandler(shortURLList))
-	err := http.ListenAndServe(":8080", nil)
+	r := chi.NewRouter()
+	r.Post("/", handlers.ShortURLHandler(shortURLList))
+	r.Get("/", handlers.ShortURLHandler(shortURLList))
+	//http.HandleFunc(, handlers.ShortURLHandler(shortURLList))
+	err := http.ListenAndServe(":8080", r)
 	if err != nil {
 		return
 	}
