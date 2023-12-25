f477d29c461e00aa16b840322538470e1fc38e5d
rstarostenko <starslan-1@yandex.ru>
2023-12-25 13:57:09 +0300

    iter5

--- internal/app/config/Config.go
+++ internal/app/config/Config.go
@@ -1,5 +1,7 @@
 package config
 
+import "github.com/caarlos0/env/v10"
+
 type Config struct {
 	ServerAddress string `env:"SERVER_ADDRESS"`
 	BaseURL       string `env:"BASE_URL"`
@@ -8,9 +10,9 @@ type Config struct {
 var cfg Config
 
 func AppConfig() Config {
-	//err := env.Parse(&cfg)
-	//if err != nil {
-	cfg = Config{ServerAddress: "8080", BaseURL: "http://localhost"}
-	//}
+	err := env.Parse(&cfg)
+	if err != nil {
+		cfg = Config{ServerAddress: "8080", BaseURL: "http://localhost"}
+	}
 	return cfg
 }
