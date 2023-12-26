27080c2286619e5b2b46a3c0b5df99be407346d5
rstarostenko <starslan-1@yandex.ru>
2023-12-26 10:28:35 +0300

    iter5

--- internal/app/config/Config.go
+++ internal/app/config/Config.go
@@ -6,7 +6,7 @@ import (
 )
 
 type Config struct {
-	ServerAddress string `env:"SERVER_ADDRESS" envDefault:"8080""`
+	ServerAddress string `env:"SERVER_ADDRESS" envDefault:"8080"`
 	BaseURL       string `env:"BASE_URL" envDefault:"http://localhost"`
 }
 
